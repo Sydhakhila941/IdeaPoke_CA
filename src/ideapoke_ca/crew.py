5b37faf955b06b08c85576f95c21238f09c018e0
Sydhakhila941 <163057747+Sydhakhila941@users.noreply.github.com>
2025-03-09 19:35:13 +0530

    Add files via upload

---
+++ src/ideapoke_ca/crew.py
@@ -0,0 +1,72 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai import LLM
+
+from crewai_tools import SerperDevTool
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+# Load environment variables
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+@CrewBase
+class IdeapokeCa():
+	"""IdeapokeCa crew"""
+
+	# Learn more about YAML configuration files here:
+	# Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
+	# Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
+	agents_config = 'config/agents.yaml'
+	tasks_config = 'config/tasks.yaml'
+	llm = LLM(model_name="gpt-4o-mini",api_key=os.getenv('OPENAI_API_KEY'))
+
+	# If you would like to add tools to your agents, you can learn more about it here:
+	# https://docs.crewai.com/concepts/agents#agent-tools
+	@agent
+	def researcher(self) -> Agent:
+		return Agent(
+            llm = self.llm,
+			config=self.agents_config['researcher'],
+            tools=[SerperDevTool()],
+			verbose=True
+		)
+
+	@agent
+	def reporting_analyst(self) -> Agent:
+		return Agent(
+            llm = self.llm,
+			config=self.agents_config['reporting_analyst'],
+			verbose=True
+		)
+
+	# To learn more about structured task outputs, 
+	# task dependencies, and task callbacks, check out the documentation:
+	# https://docs.crewai.com/concepts/tasks#overview-of-a-task
+	@task
+	def research_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['research_task'],
+		)
+
+	@task
+	def reporting_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['reporting_task'],
+			output_file='report.md'
+		)
+
+	@crew
+	def crew(self) -> Crew:
+		"""Creates the IdeapokeCa crew"""
+		# To learn how to add knowledge sources to your crew, check out the documentation:
+		# https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+
+		return Crew(
+			agents=self.agents, # Automatically created by the @agent decorator
+			tasks=self.tasks, # Automatically created by the @task decorator
+			process=Process.sequential,
+			verbose=True,
+			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+		)
